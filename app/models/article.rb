e73818657226df9c329752660a7a73859acfc8dc
francis <francis.smith@gmail.com>
2013-05-07 14:40:49 +0100

    database defined

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ActiveRecord::Base
   attr_accessible :art_title, :body, :date_posted, :subtitle, :time_posted, :user_id
+
+  belongs_to :users
+ 
+  validates :body, :length => { :maximum => 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :surname, :username
+  has_many :articles
 end
